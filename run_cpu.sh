6cb39c27f163e28c2c2e5130e16c9a4c76c92858
Niklas Britz <NikBritz@t-online.de>
2025-04-28 13:52:51 +0200

    cpu_script

---
+++ run_cpu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --chdir .
+#SBATCH --account cil_cpu
+#SBATCH --time=01:00:00
+#SBATCH -o /home/%u/logs/slurm_output__%x-%j.out
+#SBATCH --error=/home/%u/logs/slurm_err__%x-%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mem-per-cpu=3000
+
+set -e
+set -o xtrace
+echo PWD:$(pwd)
+echo STARTING AT $(date)
+
+# Environment
+source /cluster/courses/cil/envs/miniforge3/etc/profile.d/conda.sh
+conda activate /cluster/courses/cil/envs/sentiment_analysis/
+# ...
+
+# Run your experiment
+cd src
+python3 run_experiment.py -c bertweet_pretrained_inference
+# python my_script.py
+
+echo "Done."
+echo FINISHED at $(date)
