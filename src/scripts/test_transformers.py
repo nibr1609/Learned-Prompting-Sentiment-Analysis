ac326b83a10492e74f24943234eb34daebe99b80
Alexander Zank <alexander@zank.me>
2025-05-07 19:44:28 +0200

    Add LLM-via-Transformers test script.

---
+++ src/scripts/test_transformers.py
@@ -0,0 +1,50 @@
+import torch as torch
+from os.path import dirname
+from transformers import pipeline
+
+
+def make_pipeline(
+    model_dir: str,
+    pipeline_type: str = "text-to-text",
+    override_device: str = None,
+):
+    model_dir = (
+        model_dir if model_dir.startswith("/") else dirname(__file__) + "/" + model_dir
+    )
+
+    device = (
+        override_device
+        or (torch.cuda.is_available() and "cuda")
+        or (torch.mps.is_available() and "mps")
+        or "cpu"
+    )
+    print(f"Using device: {device}")
+
+    return pipeline(
+        pipeline_type,
+        model=model_dir,
+        device=device,
+        torch_dtype=torch.bfloat16,
+    )
+
+
+messages = [
+    {
+        "role": "system",
+        "content": [
+            {
+                "type": "text",
+                "text": "You are an expert in recognizin people's sentiments. Reply only with one word: positive, negative, or neutral.",
+            }
+        ],
+    },
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text": "I FUCKING HATE THIS MOVIE! it sucks so bad... is what i would say if i was a loser. But actually, I love it!",
+            },
+        ],
+    },
+]
