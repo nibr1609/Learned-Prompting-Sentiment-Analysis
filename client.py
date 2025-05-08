e580ec7fa78b62347a168ab5c9339c6fd88f3abc
Karl <deck.karli@gmail.com>
2025-05-08 10:00:27 +0200

    updated server client

--- client.py
+++ client.py
@@ -2,10 +2,13 @@ import lmql
 
 query_string = """
 "Q: What is the sentiment of the following review: ```The food was very good.```?\\n"
-"A: [SENTIMENT]" where (len(TOKENS(SENTIMENT)) < 200)
+"A: Let's think step by step. [ANALYSIS]. Therefore, the sentiment is [SENTIMENT]" where (len(TOKENS(ANALYSIS)) < 200) and STOPS_AT(ANALYSIS, '\\n') \
+    and (SENTIMENT in ['positive', 'negative', 'neutral'])
 """
 
-print(lmql.run_sync(
-    query_string, 
-    model = lmql.model("llama.cpp:/Users/deckk/CIL/CILProject2025/zephyr-7b-beta.Q4_K_M.gguf", 
-        tokenizer = 'HuggingFaceH4/zephyr-7b-beta')).variables['SENTIMENT'])
+model = lmql.model("llama.cpp:/Users/deckk/CIL/CILProject2025/zephyr-7b-beta.Q4_K_M.gguf",
+       tokenizer = 'HuggingFaceH4/zephyr-7b-beta')
+
+out = lmql.run_sync(query_string, model = model).variables['SENTIMENT']
+
+print(out)
--- server.py
+++ server.py
@@ -1,3 +1,3 @@
 import lmql
 
-lmql.serve("llama.cpp:/Users/deckk/CIL/CILProject2025/zephyr-7b-beta.Q4_K_M.gguf", port=8080, trust_remote_code=True, n_ctx=4096)
+lmql.serve("llama.cpp:/Users/deckk/CIL/CILProject2025/zephyr-7b-beta.Q4_K_M.gguf", port=8080, trust_remote_code=True, n_ctx=4096, device_map= 'auto', verbose=True)
